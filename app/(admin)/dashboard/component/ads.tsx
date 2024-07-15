584ddc5e98ebf8a20139c646cc555bab66b9038e
joseenricoaquino <joseenrico.aquino@benilde.edu.ph>
2024-07-16 06:05:27 +0800

    advertisment image

--- app/(admin)/dashboard/component/ads.tsx
+++ app/(admin)/dashboard/component/ads.tsx
@@ -9,16 +9,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import Image from "next/image";
+import ads from "@/public/mcdo ads.jpg";
+import Link from "next/link";
 const AdvertisementCard = () => {
   return (
-    <Card className="min-h-[16rem]">
-      <CardHeader className="pb-3">
-        <CardTitle className="text-center text-sm">Advertisement</CardTitle>
-      </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter></CardFooter>
-    </Card>
+    <Link href={"google.com"} target="_blank">
+      <div className="w-full h-[16rem] relative">
+        <Card className="h-full bg-transparent">
+          <CardHeader className="pb-3">
+            <CardTitle className="text-center text-sm relative z-10">
+              Advertisement
+            </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+        <Image src={ads} alt="" fill className="object-fit object-center" />
+      </div>
+    </Link>
   );
 };
 
