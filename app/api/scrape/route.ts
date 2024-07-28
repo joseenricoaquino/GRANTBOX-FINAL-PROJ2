37df7164a1b1f8ea7db37009f2b2810f58ec4022
joseenricoaquino <joseenrico.aquino@benilde.edu.ph>
2024-07-28 22:18:19 +0800

    prevent deletion of formlink when scraping

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -21,7 +21,7 @@ interface Scraped {
   eligibility: string;
   deadline: string;
   url: string;
-  formLink: string;
+  formLink: string | null;
   gwa: string | undefined;
   financial: string | undefined;
   citizenship: string | undefined;
@@ -475,6 +475,11 @@ async function handleFEUScrape(url: string, university: UniversityEnum) {
 
   console.log(scholarshipData);
 
+    // Fetch existing scholarships for merging
+    const existingScholarships = await prisma.scholarship.findMany({
+      where: { collegeId: existingCollege?.id, sourceType: "SCRAPED" },
+    });
+
   for (const scholarship of scholarshipData) {
     try {
       //console.log(`Navigating to URL: ${scholarship.url}`);
@@ -587,13 +592,18 @@ async function handleFEUScrape(url: string, university: UniversityEnum) {
         }
       });
 
+          // Find existing scholarship with the same title
+          const existingScholarship = existingScholarships.find(
+            (sch) => sch.title === scholarship.title
+          );
+
       const newScholarship: Scraped = {
         title: scholarship.title,
         description: benefits.join("\n").trim(),
         benefits: benefits.join(" ").replace(/\n/g, " ").trim(),
         eligibility: eligibility.join(" ").replace(/\n/g, " ").trim(),
         url: scholarship.url,
-        formLink: "",
+        formLink: existingScholarship ? existingScholarship.formLink : "NA",
         deadline: "N/A",
         gwa,
         financial,
@@ -604,7 +614,7 @@ async function handleFEUScrape(url: string, university: UniversityEnum) {
 
       scholarDataScrape.push(newScholarship);
 
-      // Optional: Add a delay between requests to be polite to the server
+      // Optional: Add a delay between requests to be manage to the server
       await new Promise((resolve) => setTimeout(resolve, 1000));
     } catch (error) {
       console.error(`Error scraping ${scholarship.url}:`, error);
@@ -667,10 +677,10 @@ export async function POST(request: Request) {
     const {} = body;
 
     const UNIVERSITIES: UniversityEnum[] = [
-     "De La Salle Benilde",
-     "Far Eastern University",
-     "Colegio de San Juan de Letran",
-     "Ateneo de Manila University",
+      "De La Salle Benilde",
+      "Far Eastern University",
+      "Colegio de San Juan de Letran",
+      "Ateneo de Manila University",
     ];
 
     const allScholarships = await Promise.all(
